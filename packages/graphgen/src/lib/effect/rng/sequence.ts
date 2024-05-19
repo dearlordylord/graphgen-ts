37a5870094e48a1ff1a2e75210933844b56bfe01
Igor Loskutov <igor.loskutoff@gmail.com>
2024-05-19 18:45:50 -0400

    wire in stateless state assertions into effect Random service

--- packages/graphgen/src/lib/effect/rng/sequence.ts
+++ packages/graphgen/src/lib/effect/rng/sequence.ts
@@ -49,6 +49,7 @@ import { assertExists } from '@firfi/utils/index';
 import { rngStateFromSeed } from '@firfi/utils/rng/seed/seed';
 import { castSeed } from '@firfi/utils/rng/seed/types';
 import { hash } from '@firfi/utils/string';
+import * as TU from 'fp-ts/Tuple';
 
 // to eliminate the connaiscence of random generator algorithms
 const randomFromState = (state: RngState) =>
@@ -58,17 +59,35 @@ const stateFromSeed = rngStateFromSeed;
 export class Random extends Context.Tag('Random')<
   Random,
   {
+    readonly getState: Effect.Effect<RngState>;
     readonly next: Effect.Effect<number>;
+    readonly nextStateful: (state: RngState) => Effect.Effect<[number, RngState]>;
   }
 >() {
   static Live = (seed: number) => {
     let rng = pipe(seed, castSeed, stateFromSeed, randomFromState);
+    const getState = (rng_: typeof rng): RngState => assertExists(rng_.getState, 'getState supposed to be here').bind(rng_)();
     return {
+      getState: Effect.sync(() => getState(rng)),
       next: Effect.sync(() => {
         const [next, rng1] = rng.next();
         rng = rng1;
         return next;
       }),
+      // asserts state state is not desynchronized
+      nextStateful: (state: RngState) => Effect.sync((): [number, RngState] => {
+          const currentState = getState(rng);
+          // compare two number[] arrays
+          for (let i = 0; i < currentState.length; i++) {
+            if (currentState[i] !== state[i]) {
+              throw new Error('nextStateful: state mismatch');
+            }
+          }
+          const [next, rng1] = rng.next();
+          rng = rng1;
+          return [next, getState(rng)];
+        }
+      )
     }
   };
 }
@@ -112,13 +131,24 @@ export const randomIntEffect = Effect.gen(function* () {
   return yield* random.next;
 });
 
+export const randomIntStatefulEffect = (state: RngState) => Effect.gen(function* () {
+  const random = yield* Random;
+  return yield* random.nextStateful(state)
+});
+
 // scale the random number to [0, 1); no negatives
 export const random01Effect = Effect.map(
   randomIntEffect,
   flow(castInteger, intTo01)
 );
 
+export const random01StatefulEffect = (state: RngState) =>
+  Effect.map(randomIntStatefulEffect(state), TU.mapFst(flow(castInteger, intTo01)));
+
 export const random01TypedEffect = Effect.map(random01Effect, castRandom01);
+
+export const random01TypedStatefulEffect = (state: RngState) =>
+  Effect.map(random01StatefulEffect(state), TU.mapFst(castRandom01));
 
 export const random0255Effect = Effect.map(random01Effect, (n) =>
   Math.floor(n * 256)
@@ -168,11 +198,13 @@ export const uuidForPairEffect =
 export const graphStream = (
   settings: GraphGeneratorSettingsInput = defaultSettingsInput
 ): Stream.Stream<GraphGeneratorItem, never, Random> => Stream.fromIterableEffect(Effect.gen(function* () {
+  const random = yield* Random;
   // recommendation https://discord.com/channels/795981131316985866/1241860145286348860/1241861616686071890
   const runtime = yield* Effect.runtime<Random>()
   const runSync = Runtime.runSync(runtime);
-  const getRandom = (_stateStub: RngState): [Random01, RngState] => [runSync(random01TypedEffect), _stateStub];
-  return defGenerateGraph(settings)(getRandom)(stateFromSeed(castSeed(hash(`doesn't matter`))))();
+  const state0 = yield* random.getState;
+  const getRandom = (state: RngState): [Random01, RngState] => runSync(random01TypedStatefulEffect(state));
+  return defGenerateGraph(settings)(getRandom)(state0)();
 }));
 
 const prints = Stream.run(
